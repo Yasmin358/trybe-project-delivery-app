195665a27512af1631990e3d26a72339bc44f8d3
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-01-23 22:56:15 -0300

    Criada a função para realizar a soma dos produtos do carrinho de compras

--- front-end/src/components/ButtonCart.jsx
+++ front-end/src/components/ButtonCart.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../context/CartContext';
 
-function ButtonCart(totalPrice) {
+function ButtonCart() {
+  const { cart } = useContext(CartContext);
+  const productCart = [...cart];
+  // console.log(productCart);
+  const totalPrice = () => {
+    const productCartSum = productCart.reduce(
+      (acc, curr) => Number((acc + curr.value).toFixed(2)),
+      0,
+    );
+    return productCartSum;
+  };
   return (
     <div>
       <button data-testid="customer_products__button-cart" type="button">
         <p data-testid="customer_products__checkout-bottom-value">
-          {`Ver carrinho: R$${totalPrice}`}
+          {`Ver carrinho: R$${totalPrice()}`}
         </p>
       </button>
     </div>
