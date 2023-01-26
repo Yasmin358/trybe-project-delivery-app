fd6b52b4d8d42185da1c6b98c7f39b07b2b401e4
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-01-26 19:18:32 -0300

    Corrigido os erros de lint

--- back-end/src/controllers/products.controller.js
+++ back-end/src/controllers/products.controller.js
@@ -3,7 +3,7 @@ const productsService = require('../services/products.service');
 const getAllProducts = async (_req, res) => {
   const allProducts = await productsService.findAllProducts();
   return res.status(200).json(allProducts);
-}
+};
 
 module.exports = {
   getAllProducts,
--- back-end/src/services/products.service.js
+++ back-end/src/services/products.service.js
@@ -1,10 +1,11 @@
 const models = require('../database/models');
+
 const { Product } = models;
 
 const findAllProducts = async () => {
   const allProducts = await Product.findAll({});
   return allProducts;
-}
+};
 
 module.exports = {
   findAllProducts,
