da3f47ae49f7fdf66da179a910acb869617d9915
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-01-26 22:57:22 -0300

    Configurado o ProductsProvider e o ProductsContext

---
+++ front-end/src/context/ProductsContext.jsx
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+const initialState = [];
+
+const ProductsContext = createContext(initialState);
+
+export default ProductsContext;
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import ButtonCart from '../components/ButtonCart';
 import Navbar from '../components/Navbar';
 import ProductCard from '../components/ProductCard';
+import ProductsContext from '../context/ProductsContext';
 import CartProvider from '../context/CartProvider';
-import drinksMock from '../mocks/drinks';
+// import drinksMock from '../mocks/drinks';
 
 function CustomerProducts() {
+  const { products, setProducts } = useContext(ProductsContext);
+
+  // useEffect(() => {
+  //   if (products.length === 0) {
+  //     getProducts();
+  //   }
+  // }, [products, getProducts]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/products').then((response) => {
+      setProducts(response.data);
+    });
+  }, [setProducts]);
+
+  console.log(products);
+
   return (
     <CartProvider>
       <div>
         <Navbar />
-        {drinksMock.map((drink) => (
+        {/* {drinksMock.map((drink) => (
+          <ProductCard
+            key={ drink.id }
+            id={ drink.id }
+            name={ drink.name }
+            price={ drink.price }
+            urlImage={ drink.urlImage }
+          />
+        ))} */}
+        { console.log(products) }
+        { products.map((drink) => (
           <ProductCard
             key={ drink.id }
             id={ drink.id }
