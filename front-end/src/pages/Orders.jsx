4f6dc5ac3756aa5b0902386a2256d2711db045f8
Vinícius <viniciusgolfera@gmail.com>
2023-01-28 18:54:11 -0300

    Finalizando a pagina de pedidos do cliente

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../components/Navbar';
 import OrderCard from '../components/OrderCard';
 
@@ -8,7 +9,7 @@ export default function Orders() {
 
   const getOrders = async () => {
     const { id } = JSON.parse(localStorage.getItem('user'));
-    await axios.get('http://localhost:3001/sales', { params: { id } })
+    await axios.get('http://localhost:3001/sales/customer', { id })
       .then((result) => result.data)
       .then((data) => setOrders([...data]))
       .catch((err) => {
@@ -26,14 +27,18 @@ export default function Orders() {
       <NavBar />
       {orders.length
         ? orders.map((order, numb) => (
-          <OrderCard
-            key={ order.id }
-            numb={ numb + 1 }
-            id={ order.id }
-            status={ order.status }
-            price={ order.totalPrice }
-            date={ order.saleDate }
-          />
+          <Link
+            key={ numb }
+            to={ `/customer/orders/${order.id}` }
+          >
+            <OrderCard
+              order={ order }
+              status="customer_orders__element-delivery-status"
+              date="customer_orders__element-order-date"
+              price="customer_orders__element-card-price"
+              delivery="customer_orders__element-order-id"
+            />
+          </Link>
         ))
         : <h1 className={ styles.title }>Não perca tempo, faça seu pedido!</h1>}
 
