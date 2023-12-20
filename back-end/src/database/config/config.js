f274bbc4abc5c83d3a07ed34fd9494cab15f5bb0
Yasmin358 <amorin.yasmin378@gmail.com>
2023-12-20 18:27:38 -0300

    Atualizando config.js 2

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost' || process.env.MYSQLHOST,
-  port: process.env.MYSQL_PORT || '3306' || process.env.MYSQLPORT,
-  database:
-    `${process.env.MYSQL_DB_NAME || process.env.MYSQLDATABASE || 'delivery-app'}${suffix[environment] || suffix.test} `,
-  username: process.env.MYSQL_USER || 'root' || process.env.MYSQLUSER,
-  password: process.env.MYSQL_PASSWORD || 'password' || process.env.MYSQLPASSWORD,
+  host:  process.env.MYSQLHOST || process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  port:  process.env.MYSQLPORT || process.env.MYSQL_PORT || '3306',
+  database: process.env.MYSQLDATABASE ||
+    `${process.env.MYSQL_DB_NAME  || 'delivery-app'}${suffix[environment] || suffix.test} `,
+  username: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
